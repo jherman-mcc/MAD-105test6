fc897f57d849a5e09f983cb496b3320019a9d070
JH <https://github.com/jherman-mcc/MAD-105aboutme.git>
2021-07-10 20:32:10 -0500

    Initial commit

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,16 @@
+package com.example.helloworld3
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun main(){
+        println("Hello World")
+    }
+}
+
+fun main(){
+        println("Hello World")
+    }
